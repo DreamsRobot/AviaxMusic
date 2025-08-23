0aab249b9a7ebc3f64a99af9c66e71f60377e5ee
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-08-23 10:45:44 +0530

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -155,6 +155,366 @@ HELP_16 = """
 
 # help menu
 
-HELP_17
+HELP_17 = """Here is the help for Admin:
+/ban - Ban A User
+/dban - Delete the replied message banning its sender
+/tban - Ban A User For Specific Time
+/unban - Unban A User
+/listban - Ban a user from groups listed in a message
+/listunban - Unban a user from groups listed in a message
+/warn - Warn A User
+/dwarn - Delete the replied message warning its sender
+/rmwarns - Remove All Warning of A User
+/warns - Show Warning Of A User
+/kick - Kick A User
+/dkick - Delete the replied message kicking its sender
+/purge - Purge Messages
+/purge [n] - Purge "n" number of messages from replied message
+/del - Delete Replied Message
+/promote - Promote A Member
+/fullpromote - Promote A Member With All Rights
+/demote - Demote A Member
+/pin - Pin A Message
+/mute - Mute A User
+/tmute - Mute A User For Specific Time
+/unmute - Unmute A User
+/ban_ghosts - Ban Deleted Accounts
+/report | @admins | @admin - Report A Message To Admins.
+/invite - Send Group/SuperGroup Invite Link."""
 
+HELP_18 = """ Here is the help for Admin Miscs:
+
+/set_chat_title - Change The Name Of A Group/Channel.
+/set_chat_photo - Change The PFP Of A Group/Channel.
+/set_user_title - Change The Administrator Title Of An Admin
+Here is the help for AntiService:
+/antiservice [enable|disable]. 
+
+"""
+
+HELP_19 = """ Here is the help for Autoapprove:
+
+command: /autoapprove
+
+This module helps to automatically accept chat join request send by a user through invitation link of your group
+
+Modes:
+¤ Automatic - Automatically accepts chat join request.
+
+¤ Manual - A message will be send to the chat by tagging the admins. The admins can accept or decline the requests.
+
+Use: /clear_pending Command to remove all pending user ID from DB. This will allow the user to send request again.
+ """
+
+HELP_20 = """ Here is the help for Blacklist:
+
+/blacklisted - Get All The Blacklisted Words In The Chat.
+/blacklist [WORD|SENTENCE] - Blacklist A Word Or A Sentence.
+/whitelist [WORD|SENTENCE] - Whitelist A Word Or A Sentence.
+Here is the help for Blacklist Chat:
+
+THIS MODULE IS ONLY FOR DEVS
+
+Use this module to make the bot leave some chats
+in which you don't want it to be in.
+
+/blacklist_chat [CHAT_ID] - Blacklist a chat.
+/whitelist_chat [CHAT_ID] - Whitelist a chat.
+/blacklisted - Show blacklisted chats.
+"""
+
+HELP_21 = """ Here is the help for ChatBot:
+
+/chatbot [ENABLE|DISABLE] To Enable Or Disable ChatBot In Your Chat.
+
+There's one module of this available for userbot also
+check userbot module help.
+/crypto [currency]
+        Get Real Time value from currency given.
+/dice
+    Roll a dice.
+    """
+
+HELP_22 = """ FEDRATION 
+👑 Fed Owner Only:
+ • /newfed <fed_name>: Creates a Federation, One allowed per user
+ • /renamefed <fed_id> <new_fed_name>: Renames the fed id to a new name
+ • /delfed <fed_id>: Delete a Federation, and any information related to it. Will not cancel blocked users
+ • /myfeds: To list the federations that you have created
+ • /fedtransfer <new_owner> <fed_id>:To transfer fed ownership to another person
+ • /fpromote <user>: Assigns the user as a federation admin. Enables all commands for the user under Fed Admins
+ • /fdemote <user>: Drops the User from the admin Federation to a normal User
+ • /setfedlog <fed_id>: Sets the group as a fed log report base for the federation
+ • /unsetfedlog <fed_id>: Removed the group as a fed log report base for the federation
+ • /fbroadcast : Broadcasts a messages to all groups that have joined your fed 
+
+ 🔱 Fed Admins:
+ • /fban <user> <reason>: Fed bans a user
+ • /sfban: Fban a user without sending notification to chats
+ • /unfban <user> <reason>: Removes a user from a fed ban
+ • /sunfban: Unfban a user without sending a notification
+ • /fedadmins: Show Federation admin
+ • /fedchats <Fed_ID>: Get all the chats that are connected in the Federation
+
+ User Commands:
+• /fedinfo <Fed_ID>: Information about a federation.
+• /fedadmins <Fed_ID>: List the admins in a federation.
+• /joinfed <Fed_ID>: Join the current chat to a federation. A chat can only join one federation. Chat owners only.
+• /leavefed: Leave the current federation. Only chat owners can do this.
+• /fedstat: List all the federations that you have been banned in.
+• /fedstat <user_ID>: List all the federations that a user has been banned in.
+• /fedstat <Fed_ID>: Gives information about your ban in a federation.
+• /fedstat <user_ID> <FedID>: Gives information about a user's ban in a federation.
+• /chatfed: Information about the federation the current chat is in.
+
+"""
+
+
+HELP_23 = """ Here is the help for Filters:
+/filters To Get All The Filters In The Chat.
+/filter [FILTER_NAME] To Save A Filter(reply to a message).
+
+Supported filter types are Text, Animation, Photo, Document, Video, video notes, Audio, Voice.
+
+To use more words in a filter use.
+/filter Hey_there To filter "Hey there".
+
+/stop [FILTER_NAME] To Stop A Filter.
+/stopall To delete all the filters in a chat (permanently).
+
+You can use markdown or html to save text too.
+
+Checkout /markdownhelp to know more about formattings and other syntax.
+"""
+
+HELP_24 = """Here is the help for Flood:
+
+Anti-Flood system, the one who sends more than 10 messages in a row, gets muted for an hour (Except for admins).
+
+/flood [ENABLE|DISABLE] - Turn flood detection on or off
+
+"""
+
+HELP_25 = """Here is the help for Greetings:
+
+/captcha [ENABLE|DISABLE] - Enable/Disable captcha.
+
+/set_welcome - Reply this to a message containing correct
+format for a welcome message, check end of this message.
+
+/del_welcome - Delete the welcome message.
+/get_welcome - Get the welcome message.
+
+SET_WELCOME ->
+
+To set a photo or gif as welcome message. Add your welcome message as caption to the photo or gif. The caption muse be in the format given below.
+
+For text welcome message just send the text. Then reply with the command 
+
+The format should be something like below.
+**Hi** {name} [{id}] Welcome to {chat}
+
+~ #This separater (~) should be there between text and buttons, remove this comment also
+
+button=[Duck, https://duckduckgo.com]
+button2=[Github, https://github.com]
+NOTES ->
+
+Checkout /markdownhelp to know more about formattings and other syntax."""
+
+
+HELP_26 = """ Here is the help for Info:
+
+/info [USERNAME|ID] - Get info about a user.
+/chat_info [USERNAME|ID] - Get info about a chat.
+
+Here is the help for Inline:
+
+
+Send /inline for help related to inline.
+
+Example Usage:
+@DreamSongRobot google github"""
+
+
+HELP_27 = """Here is the help for Karma:
+[UPVOTE] - Use upvote keywords like "+", "+1", "thanks", etc to upvote a message.
+[DOWNVOTE] - Use downvote keywords like "-", "-1", etc to downvote a message.
+/karma_toggle [ENABLE|DISABLE] - Enable or Disable Karma System In Your Chat.
+Reply to a message with /karma to check a user's karma
+Send /karma without replying to any message to check karma list of top 10 users """
+
+
+HELP_28 = """ Here is the help for Locks:
+
+Commands: /lock | /unlock | /locks [No Parameters Required]
+
+Parameters:
+    messages | stickers | gifs | media | games | polls
+
+    inline  | url | group_info | user_add | pin
+
+You can only pass the "all" parameter with /lock, not with /unlock
+
+Example:
+    /lock all"""
+
+
+HELP_29 = """Here is the help for Misc:
+
+/asq
+    Ask a question
+
+/commit
+    Generate Funny Commit Messages
+
+/runs
+    Idk Test Yourself
+
+/id
+    Get Chat_ID or User_ID
+
+/random [Length]
+    Generate Random Complex Passwords
+
+/cheat [Language] [Query]
+    Get Programming Related Help
+
+/tr [LANGUAGE_CODE]
+    Translate A Message
+    Ex: /tr en
+
+/json [URL]
+    Get parsed JSON response from a rest API.
+
+/arq
+    Statistics Of ARQ API.
+
+/webss | .webss [URL] [FULL_SIZE?, use (y|yes|true) to get full size image. (optional)]
+    Take A Screenshot Of A Webpage
+
+/reverse
+    Reverse search an image.
+
+/carbon
+    Make Carbon from code.
+
+/tts
+    Convert Text To Speech.
+
+/autocorrect [Reply to a message]
+    Autocorrects the text in replied message.
+
+/pdf [Reply to an image (as document) or a group of images.]
+    Convert images to PDF, helpful for online classes.
+
+/markdownhelp
+    Sends mark down and formatting help.
+
+/backup
+    Backup database
+
+/ping
+    Check ping of all 5 DCs.
+    
+#RTFM - Tell noobs to read the manual"""
+
+
+HELP_30 = """Here is the help for Notes:
+/notes To Get All The Notes In The Chat.
+
+/save [NOTE_NAME] To Save A Note.
+
+Supported note types are Text, Animation, Photo, Document, Video, video notes, Audio, Voice.
+
+To change caption of any files use.
+/save [NOTE_NAME] [NEW_CAPTION].
+
+#NOTE_NAME To Get A Note.
+
+/delete [NOTE_NAME] To Delete A Note.
+/deleteall To delete all the notes in a chat (permanently).
+
+Checkout /markdownhelp to know more about formattings and other syntax.
+
+Here is the help for Paste:
+/paste - To Paste Replied Text Or Document To A Pastebin"""
+
+
+HELP_31 = """Here is the help for Pipes:
+
+THIS MODULE IS ONLY FOR DEVS
+
+Use this module to create a pipe that will forward messages of one chat/channel to another.
+
+
+/activate_pipe [FROM_CHAT_ID] [TO_CHAT_ID] [BOT|USERBOT]
+
+    Active a pipe.
+
+    choose 'BOT' or 'USERBOT' according to your needs,
+    this will decide which client will fetch the
+    message from 'FROM_CHAT'.
+
+
+/deactivate_pipe [FROM_CHAT_ID]
+    Deactivete a pipe.
+
+
+/show_pipes
+    Show all the active pipes.
+
+NOTE:
+    These pipes are only temporary, and will be destroyed
+    on restart.
+
+Here is the help for Quotly:
+
+/q - To quote a message.
+/q [INTEGER] - To quote more than 1 messages.
+/q r - to quote a message with it's reply
+
+Use .q to quote using userbot    """
+
+
+HELP_32 = """Here is the help for RSS:
+
+/add_feed [URL] - Add a feed to chat
+/rm_feed - Remove feed from chat
+
+Note:
+    - This will check for updates every 5 minutes.
+    - You can only add one feed per chat.
+    - Currently RSS and ATOM feeds are supported
+    
+    """
+
+
+HELP_33 = """Here is the help for Rules:
+
+ • /rules: get the rules for this chat.
+
+Admins only:
+ • /setrules: Reply to a message to set the rules for the chat.
+ • /clearrules: clear the rules for this chat.
+ 
+ Here is the help for Shippering:
+/detect_gay - To Choose Couple Of The Day
+
+Here is the help for Stickers:
+
+/sticker_id
+    To get FileID of a Sticker.
+/get_sticker
+    To get sticker as a photo and document.
+/kang
+    To kang a Sticker or an Image.
+
+Here is the help for Telegraph:
+/telegraph [Page name]: Paste styled text on telegraph.
+
+Here is the help for iplookup:
+
+/iplookup [ip address] to get the details about that ip
+    """
 
