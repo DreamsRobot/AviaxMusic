7b1bc79d7272ff6549281a91436bc20fe7b2a6ca
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-08-24 04:25:50 +0530

    Update help.py

--- AviaxMusic/utils/inline/help.py
+++ AviaxMusic/utils/inline/help.py
@@ -66,3 +66,21 @@ def help_advanced_menu(_):
             [InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="help_main")],
         ]
     )
+
+def support_menu(_):
+    buttons = [
+        [
+            InlineKeyboardButton(text=_["B_S_2_BTN1"], url="https://t.me/CFCBots"),
+            InlineKeyboardButton(text=_["B_S_2_BTN2"], url="https://t.me/CloseFriendsCommunity"),
+        ],
+        [
+            InlineKeyboardButton(text=_["B_S_2_BTN3"], url="https://t.me/emixlove"),
+        ],
+        [
+            InlineKeyboardButton(text=_["B_S_2_BTN4"], callback_data="help_callback hb22"),
+        ],
+        [
+            InlineKeyboardButton(text="🔙 Back", callback_data="help_callback back"),
+        ],
+    ]
+    return InlineKeyboardMarkup(buttons)
